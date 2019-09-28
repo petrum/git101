be59208c864b3682f64971cb86cf7d9061f8660d
Petru Marginean <petru.marginean@gmail.com>
2019-09-28 13:26:14 -0400

    misc

--- push-force/push2-fail.sh
+++ push-force/push2-fail.sh
@@ -16,8 +16,13 @@ git push
 
 switchTo client1
 git reset --hard HEAD@{1}
-git commit -am'Removed the pusblished work1 file'
-git pull
+#git pull
+git push -f
+ls -ltr
+switchTo client2
+touch work2-2
+git add .
+git commit -am'Added a work2-2 file'
 git push
-
+ls -ltr
 echo DONE
