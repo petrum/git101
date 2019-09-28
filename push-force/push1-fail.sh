b8f74d1debdcf47bf04cf8b77ca72dfd4a700a02
Petru Marginean <petru.marginean@gmail.com>
2019-09-28 13:14:01 -0400

    misc

--- push-force/push1-fail.sh
+++ push-force/push1-fail.sh
@@ -10,5 +10,6 @@ switchTo client1
 touch work1
 git add .
 git commit -am'Added a work1 file'
+#git pull --no-edit
 git push
 echo DONE
---
+++ push-force/push2-fail.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+. ./init.sh
+
+switchTo client1
+touch work1
+git add .
+git commit -am'Added a work1 file'
+git push
+
+switchTo client2
+git pull
+touch work2
+git add .
+git commit -am'Added a work2 file'
+git push
+
+switchTo client1
+git reset ^HEAD --hard
+git commit -am'Removed the pusblished work1 file'
+
+git push
+
+echo DONE
