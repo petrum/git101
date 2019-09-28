fa0973b79acb8013bf86c7715c64cda97fed6580
Petru Marginean <petru.marginean@gmail.com>
2019-09-28 11:44:28 -0400

    misc

--- push-force/init.sh
+++ push-force/init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+cd
 GIT_BARE_DIR=~/tmp/repo.git
-
 ssh nuc rm -fr $GIT_BARE_DIR
 ssh nuc mkdir -p $GIT_BARE_DIR
 #ls -ltr $GIT_BARE_DIR
 
 ssh nuc "cd $GIT_BARE_DIR; git init --bare"
+mkdir tmp
+cd ~/tmp
+rm -fr repo
+git clone ssh://petrum@nuc/home/petrum/tmp/repo.git
