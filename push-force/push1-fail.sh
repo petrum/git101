8db22cd92b854721141a1554cc44dc23afe3ffcd
Petru Marginean <petru.marginean@gmail.com>
2019-09-28 12:57:52 -0400

    misc

---
+++ push-force/push1-fail.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+. ./init.sh
+
+switchTo client2
+touch work2
+git add .
+git commit -am'Added a work2 file'
+git push
+switchTo client1
+touch work1
+git add .
+git commit -am'Added a work1 file'
+#git pull --no-edit
+git push
+
+echo DONE
