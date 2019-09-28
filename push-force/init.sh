996faac71a2b188b4561bfc04cfdc360b16ee292
Petru Marginean <petru.marginean@gmail.com>
2019-09-28 11:33:11 -0400

    misc

--- push-force/init.sh
+++ push-force/init.sh
@@ -2,9 +2,8 @@
 
 GIT_BARE_DIR=~/tmp/repo.git
 
-rm -fr $GIT_BARE_DIR
-mkdir -p $GIT_BARE_DIR
+ssh nuc rm -fr $GIT_BARE_DIR
+ssh nuc mkdir -p $GIT_BARE_DIR
 #ls -ltr $GIT_BARE_DIR
 
-cd $GIT_BARE_DIR
-git init --bare
+ssh nuc "cd $GIT_BARE_DIR; git init --bare"
