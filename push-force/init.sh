1b6c6c39877ac2f1c59c7b6214978b7046ed2502
Petru Marginean <petru.marginean@gmail.com>
2019-09-28 13:01:42 -0400

    misc

--- push-force/init.sh
+++ push-force/init.sh
@@ -32,15 +32,5 @@ git add .
 git commit -am'Added a README file'
 git push
 cloneRepo repo.git client2
-touch work2
-git add .
-git commit -am'Added a work2 file'
-git push
-switchTo client1
-touch work1
-git add .
-git commit -am'Added a work1 file'
-git pull --no-edit
-git push
 
-echo DONE
+echo DONE init
