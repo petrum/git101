d54ef4548f82f0ae6f351d3c364fd2a986a679c0
Petru Marginean <petru.marginean@gmail.com>
2019-09-29 17:01:58 -0400

    misc

--- push-force/init.sh
+++ push-force/init.sh
@@ -5,7 +5,6 @@ set -e
 cd
 function createRemoteRepo {
     GIT_BARE_DIR=$ROOT_DIR/$1
-    rm -fr $GIT_BARE_DIR
     mkdir -p $GIT_BARE_DIR
     cd $GIT_BARE_DIR
     git init --bare
@@ -14,7 +13,6 @@ function createRemoteRepo {
 function cloneRepo {
     REMOTE_NAME=$ROOT_DIR/$1
     LOCAL_NAME=$ROOT_DIR/$2
-    rm -fr $LOCAL_NAME
     git clone $REMOTE_NAME $LOCAL_NAME
     cd $LOCAL_NAME
 }
@@ -29,7 +27,8 @@ function commitNewFile {
     git add $FILE
     git commit -am"Added the '$FILE' file"    
 }
-
+rm -fr $ROOT_DIR
+mkdir -p $ROOT_DIR
 createRemoteRepo server.git
 cloneRepo server.git client1
 touch README
